8994835e89c9c39cab9b1eb7e1d112bd8c399912
Abluceli <787873309@qq.com>
2019-09-17 14:52:46 +0800

    socket show

---
+++ Scripts/show_xiaqi/UDPManagments.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+//引入库
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class UdpManagment
+{
+
+    //服务器端
+    //public Dictionary<int, UdpClient> port_sockets_server;
+    //static public Dictionary<int, Control> port_control = new Dictionary<int, Control>();
+    public string self_ip;
+    //public string ServerIP;
+    public int myPort;
+    //public Scenes sence;
+    //public List<Control> controls;
+   // public bool setSence = false;
+    public UdpClient recserver;
+
+    //"111.186.116.169"), 8001
+    public UdpManagment(string ip, int port)//,int senport
+    {
+        //this.controls = new List<Control>();
+        //this.port_sockets_server = new Dictionary<int, UdpClient>();
+        this.self_ip = ip;
+        //this.ServerIP = ServerIP;
+        this.myPort = port;
+    }
+
+    public void startServer()
+    {
+        ParameterizedThreadStart s = new ParameterizedThreadStart(this.ReciveMsg);
+        Thread t = new Thread(s);
+        //recserver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        //recserver.Bind(new IPEndPoint(IPAddress.Parse(self_ip), myPort));//绑定端口号和IP
+        recserver = new UdpClient(new IPEndPoint(IPAddress.Parse(self_ip), myPort));
+        t.IsBackground = true;
+        t.Start(recserver);
+    }
+
+    /*public void start()
+    {
+        ParameterizedThreadStart s = new ParameterizedThreadStart(this.ReciveMsg);
+        foreach (int key in this.port_sockets_server.Keys)
+        {
+            Thread t = new Thread(s);
+            t.IsBackground = true;
+            t.Start(this.port_sockets_server[key]);
+        }
+    }*/
+
+    /// <summary>  
+    /// 向特定ip的主机的端口发送数据报 
+    /// "111.186.116.189"), 8001
+    /// </summary>  
+    public void sendMsg(object obj, IPEndPoint point, string type)
+    {
+        string msg = "";
+        if (msg != "")
+        {
+            string msg_new = type + "#" + msg;
+            //recserver.SendTo(Encoding.UTF8.GetBytes(msg_new), point);
+            recserver.Send(Encoding.UTF8.GetBytes(msg_new), Encoding.UTF8.GetBytes(msg_new).Length, point);
+
+        }
+    }
+    /// <summary>  
+    /// 接收发送给本机ip对应端口号的数据报  
+    /// </summary>  
+    public void ReciveMsg(object socket)
+    {
+        while (true)
+        {
+            IPEndPoint point = new IPEndPoint(IPAddress.Any, 0);//用来保存发送方的ip和端口号  
+                                                                //byte[] buffer = new byte[4096*4];
+                                                                //int length = (socket as Socket).ReceiveFrom(buffer, ref point);//接收数据报  
+            byte[] buffer = (socket as UdpClient).Receive(ref point);//接收数据报
+            string message = Encoding.UTF8.GetString(buffer);
+            Debug.Log("message is "+ message);
+           /* if (message != "")
+            {
+                string[] msg = message.Split('#');
+                string type = msg[0];
+
+                *//*if (type == "control")
+                {
+                    //Control con = (Control)JsonConvert.DeserializeObject(msg[1], typeof(Control));
+                    // port_control.Add(int.Parse(point.ToString().Split(':')[1]), con);
+                   // port_control[int.Parse(point.ToString().Split(':')[1])] = con;
+                    // Debug.Log("xxxxxxxxx is"+int.Parse(point.ToString().Split(':')[1]));
+                }*//*
+                //int.Parse(point.ToString().Split(':')[1])
+                //this.sendMsg(con, new IPEndPoint(IPAddress.Parse(this.ServerIP), int.Parse(point.ToString().Split(':')[1])), type);
+                //this.tasks.Add(obj);
+
+
+            }*/
+
+        }
+
+
+        }
+
+    public void closeSocket()
+    {
+        this.recserver.Close();
+        /*foreach (int Key in this.port_sockets_server.Keys)
+        {
+            if (port_sockets_server[Key] != null)
+            {
+                port_sockets_server[Key].Close();
+            }
+
+        }*/
+
+    }
+}
+
+
--- Scripts/show_xiaqi/show_xiaqi.cs
+++ Scripts/show_xiaqi/show_xiaqi.cs
@@ -1,35 +1,161 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//引入库
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using UnityEngine;
+using System.Runtime.Serialization.Json;
+//treelet
+using Newtonsoft.Json;
+//treelet
+using System.Collections;
 
 public class show_xiaqi : MonoBehaviour
 {
     //-1代表黑棋，1代表白棋，0代表没有落子
     private int[,] qipanInfo = new int[59, 59];
+    private bool change = false;
+    //private ArrayList[,] qipanInfo = new ArrayList[59, 59];
     //黑棋和白棋的对象，用于生成棋子
     public GameObject heiqi;
     public GameObject baiqi;
 
-    private UdpClient udpClient;
+    private string self_ip = "58.199.160.185";
+    private int myPort = 8888;
+    private UdpClient recserver;
+
+    //记录棋盘中棋子实体
+    public GameObject[] qizis = new GameObject[3481];
+    //记录期盼中棋子的个数
+    public int qizi_num = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 59; i++)
-        {
-            for (int j = 0; j < 59; j++)
-            {
-                GameObject qizi = Instantiate(heiqi);
-                qizi.transform.position = new Vector3(i, 0.1f, j);
-            }
-        }
+        this.startServer();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(this.change)
+        {
+            drawQiPan();
+        }
+    }
+    public void drawQiPan()
+    {
+        if(this.change)
+        {
+            foreach (GameObject g in this.qizis)
+            {
+                Destroy(g);
+            }
+            qizi_num = 0;
+            for (int i = 0; i < 59; i++)
+            {
+                for (int j = 0; j < 59; j++)
+                {
+                    GameObject qizi = null;
+                    if (System.Convert.ToInt32(qipanInfo.GetValue(i, j)) == -1)
+                    {
+                        qizi = Instantiate(heiqi);
+                        qizis[qizi_num] = qizi;
+                        qizi_num++;
+                    }
+                    if (System.Convert.ToInt32(qipanInfo.GetValue(i, j)) == 1)
+                    {
+                        qizi = Instantiate(baiqi);
+                        qizis[qizi_num] = qizi;
+                        qizi_num++;
+                    }
+                    if (System.Convert.ToInt32(qipanInfo.GetValue(i, j)) == 0)
+                    {
+                        continue;
+                    }
+                    qizi.transform.position = new Vector3(i, 0.1f, j);
+                }
+            }
+        }
         
     }
+    public void startServer()
+    {
+        ParameterizedThreadStart s = new ParameterizedThreadStart(this.ReciveMsg);
+        Thread t = new Thread(s);
+        this.recserver = new UdpClient(new IPEndPoint(IPAddress.Parse(self_ip), myPort));
+        t.IsBackground = true;
+        t.Start(recserver);
+    }
+    /// <summary>  
+    /// 向特定ip的主机的端口发送数据报 
+    /// "111.186.116.189"), 8001
+    /// </summary>  
+    public void sendMsg(object obj, IPEndPoint point, string type)
+    {
+        string msg = "";
+        if (msg != "")
+        {
+            string msg_new = type + "#" + msg;
+            //recserver.SendTo(Encoding.UTF8.GetBytes(msg_new), point);
+            recserver.Send(Encoding.UTF8.GetBytes(msg_new), Encoding.UTF8.GetBytes(msg_new).Length, point);
+
+        }
+    }
+    /// <summary>  
+    /// 接收发送给本机ip对应端口号的数据报  
+    /// </summary>  
+    public void ReciveMsg(object socket)
+    {
+        while (true)
+        {
+            IPEndPoint point = new IPEndPoint(IPAddress.Any, 9999);//用来保存发送方的ip和端口号  
+                                                                //byte[] buffer = new byte[4096*4];
+                                                                //int length = (socket as Socket).ReceiveFrom(buffer, ref point);//接收数据报  
+            byte[] buffer = (socket as UdpClient).Receive(ref point);//接收数据报
+
+            string message = Encoding.UTF8.GetString(buffer);
+
+            //= JsonConvert.DeserializeAnonymousType<int[,]>(message,new int[59,59]);
+
+            //Treelet
+            int[,] Info = JsonConvert.DeserializeAnonymousType(message, new int[59, 59]);
+            //Treelet
+            for (int i = 0; i < 59; i++)
+            {
+                for (int j = 0; j < 59; j++)
+                {
+                    if(Info[i,j] == this.qipanInfo[i,j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        this.qipanInfo[i, j] = Info[i, j];
+                        this.change = true;
+                    }
+                }
+            }
 
 
+                    /* foreach (GameObject g in this.qizis)
+                     {
+                         Destroy(g);
+                     }
+                     qizi_num = 0;
+                     this.drawQiPan();*/
+
+
+            //Debug.Log("message is " + qipanInfo[1,1]);
+        }
+
+
+    }
+
+    public void closeSocket()
+    {
+        this.recserver.Close();
+        
+    }
 }
